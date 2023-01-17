069375e508c19e49f7f52d7402a021531c619abe
RRDubina <dubinaroman.dubina@gmail.com>
2023-01-17 22:18:09 +0500

    Initial Commit

---
+++ IOS8-HW-13-DubinaRoman/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS8-HW-13-DubinaRoman
+//
+//  Created by admin on 17.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
